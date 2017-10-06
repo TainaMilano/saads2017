b4cfa0dd63a01357d1c72e4f480532ee4dc6e5b8
_Milano <tainamilano@hotmail.com>
2017-10-05 22:34:34 -0300

    Inscrição online

---
+++ site/php/config.php
@@ -0,0 +1,5 @@
+<meta http-equiv="Content-Type" content="text/html, charset=utf-8">
+<?php
+$con = mysqli_connect("localhost", "root", "", "CADASTRO") or die("Não foi possível conectar com o servidor de dados!");
+
+?>
